0192eac8fa2436ec7aba1ea988c51808f21d5d54
肖玉明 <xiaoymin@foxmail.com>
2018-08-03 13:10:59 +0800

    1.7.8 released

--- swagger-bootstrap-ui/src/main/resources/webjars/bycdao-ui/cdao/swaggerbootstrapui.js
+++ swagger-bootstrap-ui/src/main/resources/webjars/bycdao-ui/cdao/swaggerbootstrapui.js
@@ -1753,7 +1753,7 @@
     SwaggerBootstrapUi.prototype.log=function (msg) {
         if(window.console){
             //正式版不开启console功能
-            console.log(msg);
+            //console.log(msg);
         }
     }
     /***
