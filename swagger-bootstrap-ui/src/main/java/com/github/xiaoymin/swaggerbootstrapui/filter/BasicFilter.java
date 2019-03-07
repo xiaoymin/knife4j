3be5ee232fbd770b6aef4fa0a02701a233a8ef2b
肖玉明 <xiaoymin@foxmail.com>
2019-03-07 21:50:25 +0800

    无效引用

--- swagger-bootstrap-ui/src/main/java/com/github/xiaoymin/swaggerbootstrapui/filter/BasicFilter.java
+++ swagger-bootstrap-ui/src/main/java/com/github/xiaoymin/swaggerbootstrapui/filter/BasicFilter.java
@@ -12,7 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sun.misc.BASE64Decoder;
 
-import javax.servlet.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
