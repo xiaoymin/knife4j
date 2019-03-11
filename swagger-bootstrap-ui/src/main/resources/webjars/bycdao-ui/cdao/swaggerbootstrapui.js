8696d0adeb4ce315dff9fd7578713377c7b1b108
肖玉明 <xiaoymin@foxmail.com>
2019-03-11 09:08:26 +0800

    1.9.1 released

--- swagger-bootstrap-ui/src/main/resources/webjars/bycdao-ui/cdao/swaggerbootstrapui.js
+++ swagger-bootstrap-ui/src/main/resources/webjars/bycdao-ui/cdao/swaggerbootstrapui.js
@@ -5440,10 +5440,10 @@
      * @param msg
      */
     SwaggerBootstrapUi.prototype.log=function (msg) {
-        if(window.console){
+        /*if(window.console){
             //正式版不开启console功能
             console.log(msg);
-        }
+        }*/
     }
     /***
      * 获取菜单元素
