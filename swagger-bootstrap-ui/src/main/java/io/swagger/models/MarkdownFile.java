45862b99fa2818b236e1e4645b5f6fd244793b13
肖玉明 <xiaoymin@foxmail.com>
2019-04-18 14:02:40 +0800

    自定义文档针对Spring MVC环境支持,代码结构优化

--- swagger-bootstrap-ui/src/main/java/io/swagger/models/MarkdownFile.java
+++ swagger-bootstrap-ui/src/main/java/io/swagger/models/MarkdownFile.java
@@ -9,7 +9,7 @@ package io.swagger.models;
 
 /***
  *
- * @since:swagger-bootstrap-ui 1.0
+ * @since:swagger-bootstrap-ui 1.9.3
  * @author <a href="mailto:xiaoymin@foxmail.com">xiaoymin@foxmail.com</a> 
  * 2019/04/17 19:34
  */
