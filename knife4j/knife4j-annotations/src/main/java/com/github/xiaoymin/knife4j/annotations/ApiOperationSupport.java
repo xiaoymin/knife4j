6df17b8eac0052e52e75d3b11444df9417eb86a1
肖玉明 <xiaoymin@foxmail.com>
2019-08-28 22:56:01 +0800

    knife4j相关注释修改

--- knife4j/knife4j-annotations/src/main/java/com/github/xiaoymin/knife4j/annotations/ApiOperationSupport.java
+++ knife4j/knife4j-annotations/src/main/java/com/github/xiaoymin/knife4j/annotations/ApiOperationSupport.java
@@ -11,7 +11,7 @@ import java.lang.annotation.*;
 
 /**
  * <p>Help Java development engineers build powerful Swagger documents</p>
- * <p>This annotation belongs to the enhanced annotation of {@link ApiOperation}, which is unique to swagger-bootstrap-ui and provides Swagger's extended attributes.</p>
+ * <p>This annotation belongs to the enhanced annotation of @ApiOperation, which is unique to swagger-bootstrap-ui and provides Swagger's extended attributes.</p>
  * @since 1.9.4
  * @author <a href="mailto:xiaoymin@foxmail.com">xiaoymin@foxmail.com</a>
  * 2019/06/06 19:26
@@ -37,12 +37,6 @@ public @interface ApiOperationSupport {
 
     /**
      * A list of {@link DynamicParameter}s available to the API operation.
-     * Only parameters annotated with {@link org.springframework.web.bind.annotation.RequestBody}  annotation are supported
-     * <code>
-     *     public void request(@RequestBody JSONObject jsonobject){
-     *
-     *     }
-     * </code>
      * @return 动态类声明
      */
     DynamicParameters params() default @DynamicParameters;
@@ -62,13 +56,6 @@ public @interface ApiOperationSupport {
      *     <li>如果存在多个层次的参数过滤,则使用名称.属性的方式,例如 ignoreParameters={"uptModel.id","uptModel.uptPo.id"},其中uptModel是实体对象参数名称,id为其属性,uptPo为实体类,作为uptModel类的属性名称</li>
      *     <li>如果参数层级只是一级的情况下,并且参数是实体类的情况下,不需要设置参数名称,直接给定属性值名称即可.</li>
      * </ul>
-     * 例如一级参数情况下：
-     * <code>
-     *     {@code @ApiOperationSupport(ignoreParameters={"id"}) }
-     *     public void api(UptModel uptModel){
-     *
-     *     }
-     * </code>
      * @return 过滤参数数组
      */
     String[] ignoreParameters() default {};
