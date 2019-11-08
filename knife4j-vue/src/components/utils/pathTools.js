1776f059d7a224922452a5129773f6940a41901a
肖玉明 <xiaoymin@foxmail.com>
2019-11-08 16:20:35 +0800

    '修改logo图标'

--- knife4j-vue/src/components/utils/pathTools.js
+++ knife4j-vue/src/components/utils/pathTools.js
@@ -1,6 +1,5 @@
 // /userinfo/2144/id => ['/userinfo','/useinfo/2144,'/userindo/2144/id']
 export function urlToList(url) {
-  console.log(url)
   const urllist = url.split("/").filter(i => i);
   return urllist.map((urlItem, index) => {
     return `/${urllist.slice(0, index + 1).join("/")}`;
