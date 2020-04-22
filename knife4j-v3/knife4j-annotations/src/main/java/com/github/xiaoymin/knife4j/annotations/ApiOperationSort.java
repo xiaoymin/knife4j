cd43c777bf425aacba0d3fe15a8b7c275efedc3f
gongyan <gongy@dtds.com.cn>
2020-04-22 23:25:13 +0800

    初始化v3

---
+++ knife4j-v3/knife4j-annotations/src/main/java/com/github/xiaoymin/knife4j/annotations/ApiOperationSort.java
@@ -0,0 +1,16 @@
+package com.github.xiaoymin.knife4j.annotations;
+
+import java.lang.annotation.*;
+
+/**
+ * Deprecated since 1.9.4,see {@link ApiOperationSupport}
+ */
+@Deprecated
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface ApiOperationSort {
+
+    int value() default Integer.MAX_VALUE;
+
+}
