450e80561b898c745ba7551835577927ce137e9c
肖玉明 <xiaoymin@foxmail.com>
2020-04-29 09:32:38 +0800

    knife4j-cloud项目立项,初始化

--- knife4j-admin/src/main/java/com/xiaominfo/swagger/cloud/Knife4jCloudApplication.java
+++ knife4j-admin/src/main/java/com/xiaominfo/swagger/cloud/Knife4jCloudApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
 
@@ -11,6 +12,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Knife4jCloudApplication {
 
