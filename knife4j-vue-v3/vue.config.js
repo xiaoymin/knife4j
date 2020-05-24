d6483b379a8cfdb3688bd6ffb0ce656ad5f7ca04
肖玉明 <xiaoymin@foxmail.com>
2020-05-24 09:42:19 +0800

    打包

--- knife4j-vue-v3/vue.config.js
+++ knife4j-vue-v3/vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
   outputDir: "dist",
   lintOnSave: false,
   productionSourceMap: false,
-  indexPath: "index.html", // 由于springdoc-open-api的文档主页是可以自由配置的, 这里直接使用index.html即可
+  indexPath: "doc.html", // 由于springdoc-open-api的文档主页是可以自由配置的, 这里直接使用index.html即可
   css: {
     loaderOptions: {
       less: {
@@ -19,7 +19,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/": {
-        target: 'http://localhost:51331/',
+        target: 'http://localhost:17812/',
         ws: true,
         changeOrigin: true
       }
--- knife4j-vue/vue.config.js
+++ knife4j-vue/vue.config.js
@@ -1,5 +1,9 @@
+const TerserPlugin = require("terser-webpack-plugin");
+// const WebpackBundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const productionGzipExtensions = ["js", "css"];
 module.exports = {
-  publicPath: "/",
+  publicPath: ".",
   assetsDir: "webjars",
   outputDir: "dist",
   lintOnSave: false,
@@ -21,5 +25,32 @@ module.exports = {
         changeOrigin: true
       }
     }
+  },
+  configureWebpack: {
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            ecma: undefined,
+            warnings: false,
+            parse: {},
+            compress: {
+              drop_console: true,
+              drop_debugger: true,
+              pure_funcs: ['console.log', 'console.debug', 'window.console.log', 'window.console.debug'] // 移除console
+            }
+          },
+        }),
+
+      ]
+    },
+    plugins: [
+      new CompressionWebpackPlugin({
+        algorithm: "gzip",
+        test: new RegExp("\\.(" + productionGzipExtensions.join("|") + ")$"),
+        threshold: 10240,
+        minRatio: 0.8
+      })
+    ]
   }
 };
