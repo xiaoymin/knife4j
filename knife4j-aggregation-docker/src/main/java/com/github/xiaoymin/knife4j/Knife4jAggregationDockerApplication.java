a16e08c36396f78d24a79b145698dae409b5c8ff
肖玉明 <xiaoymin@foxmail.com>
2020-11-24 18:44:47 +0800

    Knife4jAggregationDockerApplication

---
+++ knife4j-aggregation-docker/src/main/java/com/github/xiaoymin/knife4j/Knife4jAggregationDockerApplication.java
@@ -0,0 +1,16 @@
+package com.github.xiaoymin.knife4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiaoymin
+ */
+@SpringBootApplication
+public class Knife4jAggregationDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Knife4jAggregationDockerApplication.class, args);
+    }
+
+}
