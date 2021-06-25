89005ec0bdd8074f7c05150b0a09d6a43a1274ce
肖玉明 <xiaoymin@foxmail.com>
2021-06-25 21:17:10 +0800

    doc

---
+++ knife4j-doc/docs/.vuepress/dist/js/baidu.js
@@ -0,0 +1,12 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    var origio=window.location.origin;
+    if(origin.indexOf("doc.xiaominfo.com")>-1){
+        hm.src = "https://hm.baidu.com/hm.js?cc5bfb32738435c06e10e57219d99584";
+    }else{
+        hm.src = "https://hm.baidu.com/hm.js?1819e862ea8110abb753ba48fb1563da";
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
