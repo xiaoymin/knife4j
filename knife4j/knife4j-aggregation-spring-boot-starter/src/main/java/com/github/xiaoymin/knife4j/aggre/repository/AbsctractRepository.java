5d5c143de03e9d400e20ceb2f4deb282cd1fa363
肖玉明 <xiaoymin@foxmail.com>
2021-06-27 17:35:11 +0800

    心跳检测30s/per

--- knife4j/knife4j-aggregation-spring-boot-starter/src/main/java/com/github/xiaoymin/knife4j/aggre/repository/AbsctractRepository.java
+++ knife4j/knife4j-aggregation-spring-boot-starter/src/main/java/com/github/xiaoymin/knife4j/aggre/repository/AbsctractRepository.java
@@ -23,9 +23,9 @@ import java.util.stream.Collectors;
 public abstract class AbsctractRepository extends PoolingConnectionManager implements RouteRepository {
 
     /**
-     * 心跳检测间隔(15s)
+     * 心跳检测间隔(30s)
      */
-    protected static final Long HEART_BEAT_DURATION=15000L;
+    protected static final Long HEART_BEAT_DURATION=30000L;
 
     protected final Map<String, SwaggerRoute> routeMap=new HashMap<>();
 
