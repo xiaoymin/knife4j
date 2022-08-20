a9e9775d007a6d84563ff8b89374d4f43bbbcac6
xiaoyumin <xiaoymin@foxmail.com>
2022-08-20 18:12:11 +0800

    增加信息

--- knife4j-doc/static/js/baidu.js
+++ knife4j-doc/static/js/baidu.js
@@ -1,10 +1,13 @@
 var _hmt = _hmt || [];
-(function() {
+(function () {
     var hm = document.createElement("script");
-    var origio=window.location.origin;
-    if(origin.indexOf("doc.xiaominfo.com")>-1){
+    var origio = window.location.origin;
+    if (origin.indexOf("doc.xiaominfo.com") > -1) {
         hm.src = "https://hm.baidu.com/hm.js?cc5bfb32738435c06e10e57219d99584";
-    }else{
+    } else if (origin.indexOf("www.xiaominfo.com" > -1)) {
+        hm.src = "https://hm.baidu.com/hm.js?6a7c85e4a256cfadfab6ee2e4cdc00ec";
+    } else {
+        //gitee.io
         hm.src = "https://hm.baidu.com/hm.js?1819e862ea8110abb753ba48fb1563da";
     }
     var s = document.getElementsByTagName("script")[0];
