891632f2e9599c8da26a3ef035fea04cddf0c96a
xiaoyumin <xiaoymin@foxmail.com>
2022-12-02 23:58:40 +0800

    增加复制plugin

--- knife4j-vue/vue.config.js
+++ knife4j-vue/vue.config.js
@@ -1,6 +1,8 @@
 const TerserPlugin = require("terser-webpack-plugin");
+var path = require('path');
 // const WebpackBundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const CopyWebPackPlugin = require('copy-webpack-plugin');
 const productionGzipExtensions = ["js", "css"];
 module.exports = {
   publicPath: ".",
@@ -54,7 +56,10 @@ module.exports = {
         test: new RegExp("\\.(" + productionGzipExtensions.join("|") + ")$"),
         threshold: 10240,
         minRatio: 0.8
-      })
+      }),
+      new CopyWebPackPlugin([
+        { from: path.resolve(__dirname, 'public/oauth'), to: path.resolve(__dirname, 'dist/webjars/oauth') }
+      ])
     ]
   }
 };
