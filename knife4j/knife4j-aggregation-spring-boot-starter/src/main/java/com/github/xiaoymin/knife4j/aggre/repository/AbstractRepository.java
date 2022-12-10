c3f2affafd9d0ee6992e4169ab2cf408ee4c54d4
xiaoyumin <xiaoymin@foxmail.com>
2022-12-10 21:02:54 +0800

    fixed:修复Knife4jAggregation聚合组件在Nacos修改时会出现重复的问题Gitee#I3ZPUS

--- knife4j/knife4j-aggregation-spring-boot-starter/src/main/java/com/github/xiaoymin/knife4j/aggre/repository/AbstractRepository.java
+++ knife4j/knife4j-aggregation-spring-boot-starter/src/main/java/com/github/xiaoymin/knife4j/aggre/repository/AbstractRepository.java
@@ -63,17 +63,17 @@ public abstract class AbstractRepository extends PoolingConnectionManager implem
         }
         return new ArrayList<>();
     }
-
+    
     /**
      * Nacos用户可能存在修改服务配置的情况，需要nacosSetting配置与缓存的routeMap做一次compare，避免出现重复服务的情况出现
      * https://gitee.com/xiaoym/knife4j/issues/I3ZPUS
      * @param settingRouteIds
      */
-    protected void heartRepeatClear(List<String> settingRouteIds){
-        if (CollectionUtils.isNotEmpty(this.routeMap)&&CollectionUtils.isNotEmpty(settingRouteIds)){
-            List<String> settingCacheRouteIds = this.routeMap.entrySet().stream().filter(s->!settingRouteIds.contains(s.getKey())).map(s->s.getKey()).collect(Collectors.toList());
-            if (CollectionUtils.isNotEmpty(settingRouteIds)){
-                //缓存中移出，避免重复
+    protected void heartRepeatClear(List<String> settingRouteIds) {
+        if (CollectionUtils.isNotEmpty(this.routeMap) && CollectionUtils.isNotEmpty(settingRouteIds)) {
+            List<String> settingCacheRouteIds = this.routeMap.entrySet().stream().filter(s -> !settingRouteIds.contains(s.getKey())).map(s -> s.getKey()).collect(Collectors.toList());
+            if (CollectionUtils.isNotEmpty(settingRouteIds)) {
+                // 缓存中移出，避免重复
                 settingCacheRouteIds.forEach(s -> this.routeMap.remove(s));
             }
         }
