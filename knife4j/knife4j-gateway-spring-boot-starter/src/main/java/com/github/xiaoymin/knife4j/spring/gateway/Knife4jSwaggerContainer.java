f3c6b325b42a13e1a18b299e28466fbc827b59dc
milo-xm <2240241948@qq.com>
2023-02-26 22:48:18 +0800

    feat: gateway支持v2和v3的ui

---
+++ knife4j/knife4j-gateway-spring-boot-starter/src/main/java/com/github/xiaoymin/knife4j/spring/gateway/Knife4jSwaggerContainer.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright © 2017-2022 Knife4j(xiaoymin@foxmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.xiaoymin.knife4j.spring.gateway;
+
+import java.util.List;
+import java.util.SortedSet;
+
+/**
+ * @author <a href="milo.xiaomeng@gmail.com">milo.xiaomeng@gmail.com</a>
+ *     23/02/26 20:43
+ * @since gateway-spring-boot-starter v4.0.0
+ */
+public interface Knife4jSwaggerContainer<T extends AbstractSwaggerResource>{
+    void discover(List<String> services);
+
+    SortedSet<T> getSwaggerResource();
+}
