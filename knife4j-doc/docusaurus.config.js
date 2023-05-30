33096529331ec44a0fda7300105b089052467dcb
xiaoyumin <xiaoymin@foxmail.com>
2023-05-30 13:16:07 +0800

    docs:fixed文档

--- knife4j-doc/docusaurus.config.js
+++ knife4j-doc/docusaurus.config.js
@@ -257,11 +257,9 @@ module.exports = {
             },
             algolia: {
                 appId: '3CRIMRK623',
-                apiKey: '69d77aed70b67e21c81c6b4a38d0cf1a', // search only (public) API key
+                apiKey: 'ae4f57f208e3c7749017e09582f0b8a4', // search only (public) API key
                 indexName: 'xiaominfo',
-                algoliaOptions: {
-                    facetFilters: ['version:VERSION'],
-                },
+                debug: true
             },
         }),
 };
