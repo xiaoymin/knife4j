31721661a75f156dfe2085984ad2e11cb4765cda
xiaoyumin <xiaoymin@foxmail.com>
2023-08-01 22:37:52 +0800

    打开tabs

--- knife4j-front/knife4j-ui-react/src/pages/Home.tsx
+++ knife4j-front/knife4j-ui-react/src/pages/Home.tsx
@@ -1,6 +1,8 @@
+import { useLocation } from 'react-router-dom';
 
 export default function Home() {
-
+    const location = useLocation();
+    console.log("location,", location)
     return (
         <div id="knife4j-home-page">
             <h1>我是主页!</h1>
