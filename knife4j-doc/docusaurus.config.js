89ada3be40c353f9c88cb75717f8333e30c5b404
xiaoyumin <xiaoymin@foxmail.com>
2023-08-06 10:35:22 +0800

    docs:优化内容

--- knife4j-doc/docusaurus.config.js
+++ knife4j-doc/docusaurus.config.js
@@ -11,10 +11,12 @@ module.exports = {
     trailingSlash: false,
     organizationName: 'xiaoymin',
     projectName: 'swagger-bootstrap-ui',
-    scripts: ['/js/custom.js', '/js/baidu.js', {
-        src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1589206801610969', async: true,
-        crossorigin: "anonymous"
-    }],
+    scripts: ['/js/custom.js', '/js/baidu.js'
+        // , {
+        //     src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1589206801610969', async: true,
+        //     crossorigin: "anonymous"
+        // }
+    ],
     favicon: 'img/favicon.ico',
     i18n: {
         defaultLocale: 'zh-Hans',
