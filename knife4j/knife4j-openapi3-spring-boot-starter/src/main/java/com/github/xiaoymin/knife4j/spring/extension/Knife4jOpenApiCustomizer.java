689a87b73afe483041701961265a388942b6dac7
xiaoyumin <xiaoymin@foxmail.com>
2023-12-09 16:10:31 +0800

    perf:部分场景下空指针的问题

--- knife4j/knife4j-openapi3-spring-boot-starter/src/main/java/com/github/xiaoymin/knife4j/spring/extension/Knife4jOpenApiCustomizer.java
+++ knife4j/knife4j-openapi3-spring-boot-starter/src/main/java/com/github/xiaoymin/knife4j/spring/extension/Knife4jOpenApiCustomizer.java
@@ -107,7 +107,7 @@ public class Knife4jOpenApiCustomizer implements GlobalOpenApiCustomizer {
                         }
                     });
             // 往openApi tags字段添加x-order增强属性
-            if (openApi.getTags()!=null){
+            if (openApi.getTags() != null) {
                 openApi
                         .getTags()
                         .forEach(
